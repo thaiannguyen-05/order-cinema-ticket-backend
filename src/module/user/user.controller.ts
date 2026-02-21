bde4c38f89a2da0bcd84a3664871366465553c9d
thaiannguyen-05 <thaiandev05@gmail.com>
2026-02-21 20:35:43 +0700

    update user update endpoint

--- src/module/user/user.controller.ts
+++ src/module/user/user.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Put, Req } from '@nestjs/common';
 import { UserService } from './user.service';
-
+import type { Request } from 'express';
+import type { UpdateUser } from './dto/update.user.dto';
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Put('update')
+  async updateUser(@Req() req: Request, @Body() dto: UpdateUser) {
+    const email = req.payload?.email;
+    dto.email = email!;
+    return this.userService.updateUserByEmail(dto);
+  }
 }
