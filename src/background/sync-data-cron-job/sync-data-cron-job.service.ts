7599e9dc0bac88c9e72303d3537399f53b9b0b5b
thaiannguyen-05 <thaiandev05@gmail.com>
2026-02-27 20:21:29 +0700

    add sync data movie coming + now showing + cinema nearby

--- src/background/sync-data-cron-job/sync-data-cron-job.service.ts
+++ src/background/sync-data-cron-job/sync-data-cron-job.service.ts
@@ -1,19 +1,17 @@
-import { HttpService } from '@nestjs/axios';
+import {
+  createMovieGluClient,
+  type MovieGluSdk,
+} from '@andev2005/movie-glu-sdk';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Cron } from '@nestjs/schedule';
 import { MyLogger } from '../../logger/logger.service';
 import { PrismaDatabaseService } from './prisma.database.service';
-import {
-  createMovieGluClient,
-  type MovieGluSdk,
-} from '@andev2005/movie-glu-sdk';
 @Injectable()
 export class SyncDataCronJobService {
   private readonly client: MovieGluSdk;
 
   constructor(
-    private readonly httpService: HttpService,
     private readonly configService: ConfigService,
     private readonly logger: MyLogger,
     private readonly dataBaseService: PrismaDatabaseService,
@@ -28,8 +26,24 @@ export class SyncDataCronJobService {
     const quantity = 100;
     const { cinemas } = await this.client.cinemas.nearby({ limit: quantity });
 
+    this.logger.debug(`Data cinema nearby ${JSON.stringify(cinemas)}`);
+
     for (const cinema of cinemas) {
       await this.dataBaseService.upsertCinema(cinema.cinema_id, cinema);
     }
+  }
+
+  @Cron('0 0 * * * *')
+  async syncDataFilmNowShowing() {
+    const quantity = 100;
+    this.logger.debug(`Call sync data film now showing`);
+    return await this.client.films.nowShowing({ limit: quantity });
+  }
+
+  @Cron('0 0 * * * *')
+  async syncDataFilmComingSoon() {
+    const quantity = 100;
+    this.logger.debug(`Call sync data film coming soon`);
+    return await this.client.films.comingSoon({ limit: quantity });
   }
 }
